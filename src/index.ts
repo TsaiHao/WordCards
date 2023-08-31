08307d550757373d64a9b41fc0abea49f0811284
haozaijun <haozaijun.cc@bytedance.com>
2023-08-31 12:31:03 +0800

    Refactor and streamline code in index.ts
    The index.ts file was refactored to remove unnecessary code and streamline the opening and running database functions. Extensive parts of the code, related to realm CLI and word processing were removed as they were no longer necessary. The database opening and creation process was simplified for better efficiency and readability. These changes will make the code cleaner and easier to maintain, without changing the software's end behavior.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-import readline from 'readline';
 import fetch from 'node-fetch';
 import sqlite3 from "sqlite3";
 import {open, Database} from "sqlite";
-import stream from 'stream';
 import express from 'express';
 
 const url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
 
 async function openDB() {
-    return open({
+    const db = await open({
         filename: "./words.db",
         driver: sqlite3.Database,
-    }).then(async (db) => {
-        await db.run("CREATE TABLE IF NOT EXISTS words (word TEXT PRIMARY KEY, definition TEXT)");
-        return db;
-    })
+    });
+    await db.run("CREATE TABLE IF NOT EXISTS words (word TEXT PRIMARY KEY, definition TEXT)");
+    return db;
 }
 
 async function saveWord(db: Database, word: string, definition: string) {
@@ -43,90 +40,8 @@ async function main() {
         console.log("Please set the environment variable DICT_KEY");
         process.exit(1);
     }
-    const output = new stream.Writable({
-        write(chunk, encoding, callback) {
-            callback();
-        }
-    });
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: output,
-    });
 
     const db = await openDB();
-
-    const displayWord = (headWord: string, wordJson: WordObject[]) => {
-        for (const word of wordJson) {
-            console.log(`${headWord}: [${word.fl}]`);
-            for (const def of word.shortdef) {
-                console.log(` ${def}`);
-            }
-        }
-    };
-
-    const processCommand = async (cmd: string) => {
-        if (cmd.startsWith('/')) {
-            cmd = cmd.substring(1);
-        }
-        if (cmd === 'exit') {
-            rl.close();
-            process.exit(0);
-        } else if (cmd.startsWith('remove ')) {
-            const word = cmd.substring(7).trim();
-            await removeWord(db, word);
-            console.log(`Removed ${word}`);
-        } else if (cmd.startsWith('list')) {
-            await listAllWords(db)
-            console.log(`Listed all words`);
-        }
-    }
-
-    const processWord = (word: string) => {
-        getWord(db, word).then((row) => {
-            if (row) {
-                displayWord(word, JSON.parse(row.definition));
-                loop();
-            } else {
-                const fullUrl = url + word + "?key=" + process.env["DICT_KEY"];
-                fetch(fullUrl).then(response => response.json())
-                    .then((json) => {
-                        type FetchedData = WordObject[] | string[];
-                        function isWordObjectArray(data: FetchedData): data is WordObject[] {
-                            return (data as WordObject[])[0].fl !== undefined;
-                        }
-                        if (isWordObjectArray(json as FetchedData)) {
-                            displayWord(word, json as WordObject[]);
-                            saveWord(db, word, JSON.stringify(json));
-                        } else {
-                            console.log("input word is not available, check the following: ");
-                            for (const word of json as string[]) {
-                                console.log(`  ${word}`);
-                            }
-                        }
-                        loop();
-                    })
-                    .catch(err => {
-                        console.log("Error fetching word: " + err);
-                        loop();
-                    });
-            }
-        });
-    }
-
-    function loop() {
-        console.log("please input a word or command")
-        rl.question("", (word) => {
-            word = word.trim();
-            if (word.startsWith('/')) {
-                processCommand(word).then(() => loop()).catch(err => {
-                    console.log("Error: " + err);
-                    loop();
-                });
-            } else {
-                processWord(word);
-            }
-        });
-    }
 
     let app = express();
     app.get("/word/:word", (req, res) => {
@@ -192,8 +107,6 @@ async function main() {
     app.listen(12300, () => {
         console.log("Server started on port 12300");
     });
-
-    loop();
 }
 
 main().catch(err => {

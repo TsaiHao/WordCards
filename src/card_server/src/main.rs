8e430d3625060d7a5df2a11503a93a6c94f3500c
haozaijun <zaijun521@gmail.com>
2024-01-18 20:25:49 +0800

    Fix errors in rust server

--- src/card_server/src/main.rs
+++ src/card_server/src/main.rs
@@ -1,9 +1,10 @@
 use reqwest;
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection, params, Result};
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::env;
 use std::sync::{Arc, Mutex};
+use rusqlite::DropBehavior::Panic;
 use tokio;
 use warp;
 use warp::Filter;
@@ -34,46 +35,33 @@ struct WordResponse {
 fn main() {
     let dict_key = env::var("DICT_KEY").expect("DICT_KEY not found in environment");
 
-    let conn = Connection::open("../../words.db").expect("Failed to open database");
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS words (word TEXT PRIMARY KEY, definition TEXT)",
+    let database = Connection::open("../../words.db").expect("Failed to open database");
+    database.execute(
+        "CREATE TABLE IF NOT EXISTS words (word TEXT PRIMARY KEY, definition TEXT, date TEXT)",
         [],
     )
         .expect("Failed to create table");
-    let conn = Arc::new(Mutex::new(conn));
 
-    let conn_word = conn.clone();
-    let dict_key_clone = dict_key.clone();
-    let word_route = warp::path!("word" / String)
-        .and_then(move |word: String| {
-            let conn = conn_word.clone();
-            let dict_key = dict_key_clone.clone();
-            async move {
-                let response = {
-                    let conn_lock = conn.lock().unwrap();
-                    // Clone data or perform all database operations here.
-                    get_word(&conn_lock, &word, &dict_key).await
-                };
-                Ok(warp::reply::json(&response))
-            }
-        });
+    let database = Arc::new(Mutex::new(database));
 
-    let conn_list = conn.clone();
-    let list_route = warp::path!("list").map(move || {
-        let conn_lock = conn_list.lock().unwrap();
-
-        let res = list_all_words(&conn_lock);
-
+    let database_list = database.clone();
+    let list_route = warp::path!("api" / "list").map(move || {
+        let database_list = database_list.lock().unwrap();
+        let res = list_all_words(&database_list);
         warp::reply::json(&res)
     });
 
-    let assets_route = warp::path("static").and(warp::fs::dir("../../public/static"));
-    let index_route = warp::path::end().and(warp::fs::file("../../public/index.html"));
+    let database_query = database.clone();
+    let query_route = warp::path!("api" / "word" / String).map(move |word: String| {
+        let db = database_query.lock().expect("get database failed when querying");
+        let res = query_word(&db, word);
+        match res {
+            Some(def) => warp::reply::json(&def),
+            None => panic!(),
+        }
+    });
 
-    let routes = index_route
-        .or(assets_route)
-        .or(word_route)
-        .or(list_route);
+    let routes = list_route.or(query_route);
 
     let runtime = tokio::runtime::Runtime::new().unwrap();
     runtime.block_on(async {
@@ -81,71 +69,41 @@ fn main() {
     });
 }
 
-async fn get_word(conn: &Connection, word: &str, dict_key: &str) -> WordResponse {
-    print!("received word: {}", word);
+fn list_all_words(db: &Connection) -> Vec<String> {
+    let list = db.prepare("SELECT word FROM words");
+    match list {
+        Ok(mut stat) => {
+            let iter = stat.query_map([], |row| {
+                let word: String = row.get(0).expect("get 0 failed");
+                Ok(word)
+            }).expect("query map failed");
 
-    let row = conn.query_row("SELECT * FROM words WHERE word = ?", &[word], |row| {
-        Ok((row.get::<_, String>(0)?, row.get::<_, String>(1)?))
-    });
-
-    match row {
-        Ok((_, _definition)) => WordResponse {
-            result: "duplicate".to_string(),
-            word: Some(word.to_string()),
-            definition: None,
-            message: None,
-            suggestions: None,
-        },
-        Err(_) => {
-            let full_url = format!("{}{}?key={}", DICT_URL, word, dict_key);
-            let res: Result<Vec<WordDefinition>, _> =
-                reqwest::get(&full_url).await.unwrap().json().await;
-
-            match res {
-                Ok(data) => {
-                    conn.execute(
-                        "INSERT INTO words (word, definition) VALUES (?, ?)",
-                        &[word, &serde_json::to_string(&data).unwrap()],
-                    )
-                        .unwrap();
-
-                    WordResponse {
-                        word: Some(word.to_string()),
-                        definition: Some(data),
-                        result: "new".to_string(),
-                        suggestions: None,
-                        message: None,
-                    }
-                }
-                Err(_) => {
-                    let suggestion: Vec<String> = reqwest::get(&full_url).await.unwrap().json().await.unwrap();
-
-                    WordResponse {
-                        word: None,
-                        definition: None,
-                        result: "spell_check".to_string(),
-                        suggestions: Some(suggestion),
-                        message: None,
-                    }
+            let mut words = Vec::new();
+            for word_result in iter {
+                if let Ok(word) = word_result {
+                    words.push(word);
                 }
             }
+
+            words
+        },
+        Err(e) => {
+            panic!("")
         }
     }
 }
 
-fn list_all_words(conn: &Connection) -> Vec<WordEntry> {
-    conn.prepare("SELECT * FROM words")
-        .unwrap()
-        .query_map([], |row| {
-            let word: String = row.get(0)?;
-            let definition: String = row.get(1)?;
-            println!("word: {}, def: {}", word, definition);
-            Ok(WordEntry {
-                word,
-                definition: serde_json::from_str(&definition).unwrap(),
-            })
-        })
-        .unwrap()
-        .map(|row| row.unwrap())
-        .collect()
+fn query_word(db: &Connection, word: String) -> Option<String> {
+    let mut stmt = db.prepare("SELECT definition FROM words where word = ?1")
+        .expect("prepare db failed");
+    let mut row = stmt.query_row(params![word], |row| row.get(0));
+
+    match row {
+        Ok(def) => {
+            def
+        },
+        Err(e) => {
+            panic!("");
+        }
+    }
 }
